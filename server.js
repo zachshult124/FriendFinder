e3a2f8ca5a16a8536149d4d2ffe9419a7e1a076a
zachshult <zachshult12345@gmail.com>
2018-09-11 23:50:28 -0500

    File Structure and Skeleton
    - Complete File Structure

---
+++ server.js
@@ -0,0 +1,15 @@
+//psudo code
+//pieces 
+//user survey
+
+
+
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
